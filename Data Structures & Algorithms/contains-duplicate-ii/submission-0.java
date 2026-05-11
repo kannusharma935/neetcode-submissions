807ada627392a61a932377f6e1e0018c2819c59b
Akash Sharma <72879189+kannusharma935@users.noreply.github.com>
2026-05-11 20:06:39 +0530

    Bulk sync: 14 submissions

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        for(int i=0;i<nums.length;i++){
+            int j=i+1;
+            while(j<nums.length){
+                if(nums[i]==nums[j]&& nums[i]+nums[j]<=k){
+                    return true;
+                }
+                j++;
+            }
+
+        }
+        return false;
+    }
+}
